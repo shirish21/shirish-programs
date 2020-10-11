132e46d495e716fedec5951bdb73fdca119d74b5
shirish21 <72141130+shirish21@users.noreply.github.com>
2020-10-11 22:12:16 +0530

    swapping of two nuumbers using function

---
+++ code14.c
@@ -0,0 +1,21 @@
+                      /*swapping of two numbers using function*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter any two numbers");
+scanf("%d%d",&a,&b);
+swap(&a,&b);
+printf("a=%d,b=%d",a,b);
+}
+
+
+void swap(int *x,int *y)
+{
+int t;
+t=*x;
+*x=*y;
+*y=t;
+}
